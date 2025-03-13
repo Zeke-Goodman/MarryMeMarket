5b9ff027f7aa9577ad14436689007966a330037b
Zeke-Goodman <120141215+Zeke-Goodman@users.noreply.github.com>
2025-03-13 14:52:40 -0600

    Initial commit - creating project, setting up bones of site, pushing everything

---
+++ RiderProjects/MarryMeMarket/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MarryMeMarket.Models;
+
+namespace MarryMeMarket.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Sell()
+    {
+        return View();
+    }
+
+    public IActionResult Gallery()
+    {
+        return View();
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
