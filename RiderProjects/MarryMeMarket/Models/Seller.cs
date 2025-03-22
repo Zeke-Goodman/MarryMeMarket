98bb1eb79f2fd1e372e8ef34f3d6abb6877a9948
Zeke-Goodman <120141215+Zeke-Goodman@users.noreply.github.com>
2025-03-22 11:17:37 -0600

    fix a couple styling issues, finish building form model for database, finish creating form for sellers to register, style form

--- RiderProjects/MarryMeMarket/Models/Seller.cs
+++ RiderProjects/MarryMeMarket/Models/Seller.cs
@@ -1,7 +1,15 @@
+using Microsoft.VisualBasic.CompilerServices;
+
 namespace MarryMeMarket.Models;
 
 public class Seller
 {
     public int Id { get; set; }
     public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Item { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public float? Price { get; set; }
+    public string? Location { get; set; }
 }
